b0702137f02de7d64498ecd3563ca4dffb6b00ce
chrisyoon <chrsjyoon@gmail.com>
2021-02-02 23:09:53 +1100

    remove third-party library for mailgun and add axios

--- config/config.ts
+++ config/config.ts
@@ -2,8 +2,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const mailgunBaseUrl = `https://api.mailgun.net/v3/${process.env.MAILGUN_DOMAIN}`;
+
 export default {
   port: process.env.PORT,
   mailgunKey: process.env.MAILGUN_CREDENTIAL as string,
-  mailgunDomain: process.env.MAILGUN_DOMAIN as string
-}
+  mailgunDomain: process.env.MAILGUN_DOMAIN as string,
+  mailgunBaseUrl,
+};
--- controllers/mailgun.ts
+++ controllers/mailgun.ts
@@ -1,21 +1,32 @@
-import mailgun from 'mailgun-js';
+import axios from 'axios';
 import config from '../config/config';
 
 // send email
 const sendMail = async (body: any) => {
-  console.log('mailgun key = ', config.mailgunKey);
-  const DOMAIN = config.mailgunDomain;
-  const mg = mailgun({ apiKey: config.mailgunKey, domain: DOMAIN });
-  const data = {
-    from: `Chris Yoon <chrisjyoon@${config.mailgunDomain}>`,
-    to: 'chrisjyoon@gmail.com',
-    subject: body.subject,
-    text: 'Testing some Mailgun awesomness!'
+  const apiKeyHeader = Buffer.from(`api:${config.mailgunKey}`).toString('base64');
+  const headers = {
+    'Authorization': `Basic ${apiKeyHeader}`
   };
-  console.log('send..');
+  const params = new URLSearchParams();
+  params.append('from', `Chris Yoon <chrisjyoon@${config.mailgunDomain}>`);
+  params.append('to', body.to)
+  if (body.cc) {
+    params.append('cc', body.cc);
+  }
+  if (body.bcc) {
+    params.append('bcc', body.bcc);
+  }
+  params.append('subject', body.subject)
+  params.append('text', body.text)
   try {
-    const resp = await mg.messages().send(data);
-    console.log(resp);
+    const resp = await axios({
+      method: 'post',
+      url: `${config.mailgunBaseUrl}/messages`,
+      headers,
+      data: params
+    });
+    console.log(resp.status);
+    console.log(resp.data);
   } catch (err) {
     console.error('error! : ', err);
   }
