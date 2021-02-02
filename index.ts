83af50f8490ee44ab3d898e7385280f95ac2c1d4
chrisyoon <chrsjyoon@gmail.com>
2021-02-02 21:49:42 +1100

    add body parser

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import config from './config/config';
 
 // controllers
 import home from './controllers/home';
 import email from './controllers/email';
 
 const app = express();
-const PORT = 8000;
+const PORT = config.port;
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // routing
 app.get('/', home);
