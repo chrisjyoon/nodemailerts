91c543c48df59623aa751d91923eb4f3232756f2
chrisyoon <chrsjyoon@gmail.com>
2021-02-02 21:29:34 +1100

    make failing test

---
+++ test/api.ts
@@ -0,0 +1,25 @@
+import request from 'supertest';
+import app from '../index';
+
+
+describe('GET /', () => {
+  it('should return 200 OK', (done) => {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
+
+describe('POST /email', () => {
+  let data = {
+    subject: 'This is from supertest mocha'
+  };
+  it('respond with 200 sent', (done) => {
+    request(app)
+      .post('/email')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-type', 'application/json; charset=utf-8')
+      .expect(200, done);
+  });
+});
