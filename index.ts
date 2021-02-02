ff564dfdc42e0a10c482c2478dd8e3cfaed11586
chrisyoon <chrsjyoon@gmail.com>
2021-02-02 21:42:04 +1100

    add dotenv and config

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import morgan from 'morgan';
 
 // controllers
 import home from './controllers/home';
-// import email from './controllers/email';
+import email from './controllers/email';
 
 const app = express();
 const PORT = 8000;
@@ -12,6 +12,7 @@ app.use(morgan('dev'));
 
 // routing
 app.get('/', home);
+app.post('/email', email);
 
 // start express server
 app.listen(PORT, () => {
