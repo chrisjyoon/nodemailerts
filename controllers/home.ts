96f319e4de6ea5b3771138928a76cdfd99775522
chrisyoon <chrsjyoon@gmail.com>
2021-02-05 22:39:06 +1100

    add example for .env

--- controllers/home.ts
+++ controllers/home.ts
@@ -5,18 +5,19 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.send(
     `
-    <h3>Welcom to mailer service using Express + Typescript Demo</h3>
+    <h3>Welcome to mailer service using Express + Typescript Demo</h3>
     Please check <a href="https://github.com/chrisjyoon/nodemailerts">README</a>
     <p>
     <form action="/email/" method="post">
       <div style="display: flex;flex-direction: column">
         <div style="display: flex;justify-content: end">
           <label for="email" style="width:100px">Email: </label>
-          <input id="email" type="email" name="to" style="width:200px"><br>
+          <input id="email" type="email" name="to" required style="width:200px"><br>
         </div>
         <div style="display: flex;justify-content: end">
           <label for="subject" style="width:100px">Subject: </label>
-          <input id="subject" type="text" name="subject" value="node express mailer test" style="width:200px">
+          <input id="subject" type="text" name="subject" value="node express mailer test"
+            required style="width:200px">
         </div>
       </div>
       <p></p>
